13945053439792a2bc9c10259bb6e3e05524eff5
jinju <jinju000202@gmail.com>
2023-01-29 20:26:55 +0900

    first commit

---
+++ pg_짝수와홀수/EvenOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(num) {
+    if(num % 2 == 0){
+        return 'Even'
+    } else {
+        return 'Odd'
+    }
+}
+//확인용 코드
+console.log(evenOrOdd(0));
+console.log(evenOrOdd(3));
