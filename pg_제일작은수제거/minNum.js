3412416957c7f3ceaf9546c8e408416f7ccd1377
jinju <jinju000202@naver.com>
2023-02-20 09:22:11 +0900

    제일 작은 수 제거

--- pg_제일작은수제거/minNum.js
+++ pg_제일작은수제거/minNum.js
@@ -1,6 +1,6 @@
 function solution(arr) {
     
-    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    arr.splice(arr.indexOf(Math.min(...arr)),1); //배열의 요소 중 가장 작은 값 하나 제거
     arr.length <= 1 ? answer = [-1] : answer = arr
     
     return answer
