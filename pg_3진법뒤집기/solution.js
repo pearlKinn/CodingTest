6c20b2f5c3dbc08f89533b75a81162da812a6416
jinju <jinju000202@naver.com>
2023-03-06 17:08:14 +0900

    3진법 뒤집기

--- pg_3진법뒤집기/solution.js
+++ pg_3진법뒤집기/solution.js
@@ -1,5 +1,4 @@
-	
 function solution(n) {
-    let answer = n.toString(3).split("").reverse().join("");;
-    return parseInt(answer, 3);
+    let answer = n.toString(3).split("").reverse().join(""); //어떤 수를.toString(몇진수로 변환하고 싶은지)
+    return parseInt(answer, 3); //parseInt('변환시키고자하는 문자열', 해당 수의 진수)
 }
