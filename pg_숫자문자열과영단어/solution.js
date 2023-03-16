7f17b631bb918b1f759d70f51ee244b15f054b9a
jinju <jinju000202@gmail.com>
2023-03-16 23:59:21 +0900

    수정 전

---
+++ pg_숫자문자열과영단어/solution.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = 0;
+    s.replace('zero', '0';)
+    s.replace("one", "1");
+    s.replace('two', '2');
+    s.replace('tree', '3');
+    s.replace('four', '4');
+    s.replace('five', '5');
+    s.replace('six', '6');
+    s.replace('seven', '7');
+    s.replace('eight', '8');
+    s.replace('nine', '9');
+
+    return answer;
+}
