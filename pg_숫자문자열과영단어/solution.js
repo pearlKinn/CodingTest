5b9fbe71ded346bcf3de8863066b7fbd0127bff5
jinju <jinju000202@gmail.com>
2023-03-17 00:34:49 +0900

    숫자 문자열과 영단어

--- pg_숫자문자열과영단어/solution.js
+++ pg_숫자문자열과영단어/solution.js
@@ -1,15 +1,31 @@
+// /zero/g ==> 문자열에 있는 zero를 찾아서 대치하라는 뜻의 정규 표현식
+// 'zero' ==> 첫번째로 나온 zero만 대치 -따라서 ''는 안됨
 function solution(s) {
     var answer = 0;
-    s = s.replace('zero', '0');
-    s = s.replace("one", "1");
-    s = s.replace('two', '2');
-    s = s.replace('tree', '3');
-    s = s.replace('four', '4');
-    s = s.replace('five', '5');
-    s = s.replace('six', '6');
-    s = s.replace('seven', '7');
-    s = s.replace('eight', '8');
-    s = s.replace('nine', '9');
+    
+    s = s.replace(/zero/g, 0);
+    s = s.replace(/one/g, 1);
+    s = s.replace(/two/g, 2);
+    s = s.replace(/three/g, 3);
+    s = s.replace(/four/g, 4);
+    s = s.replace(/five/g, 5);
+    s = s.replace(/six/g, 6);
+    s = s.replace(/seven/g, 7);
+    s = s.replace(/eight/g, 8);
+    s = s.replace(/nine/g, 9);
+    
     answer = Number(s);
     return answer;
+}
+// 다른 분의 풀이
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
 }
