276d83f7aac4e771e1c6588da3690bbd1e43a027
jinju <jinju000202@gmail.com>
2023-03-17 00:14:17 +0900

    수정 - 70점

--- pg_숫자문자열과영단어/solution.js
+++ pg_숫자문자열과영단어/solution.js
@@ -1,15 +1,15 @@
 function solution(s) {
     var answer = 0;
-    s.replace('zero', '0';)
-    s.replace("one", "1");
-    s.replace('two', '2');
-    s.replace('tree', '3');
-    s.replace('four', '4');
-    s.replace('five', '5');
-    s.replace('six', '6');
-    s.replace('seven', '7');
-    s.replace('eight', '8');
-    s.replace('nine', '9');
-
+    s = s.replace('zero', '0');
+    s = s.replace("one", "1");
+    s = s.replace('two', '2');
+    s = s.replace('tree', '3');
+    s = s.replace('four', '4');
+    s = s.replace('five', '5');
+    s = s.replace('six', '6');
+    s = s.replace('seven', '7');
+    s = s.replace('eight', '8');
+    s = s.replace('nine', '9');
+    answer = Number(s);
     return answer;
 }
