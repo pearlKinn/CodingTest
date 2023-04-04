983d599df97de6a998b8da531a5f5c6c0cc430ec
jinju <jinju000202@naver.com>
2023-04-04 15:58:31 +0900

    소수 만들기

---
+++ pg_소수만들기/solution.js
@@ -0,0 +1,27 @@
+function solution(nums) {
+    let answer = 0;
+    let arr = [];
+
+    for(let i = 0; i < nums.length-2; i++) {
+        for(let j = i + 1; j < nums.length-1; j++) {
+            for(let k = j + 1; k < nums.length; k++) {
+                if(isPrime(nums[i] + nums[j] + nums[k])) {
+                    answer++;
+                }
+            }
+        }
+    }
+
+     return answer;
+}
+
+//   함수를 따로 만들어서 소수판별
+const isPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
