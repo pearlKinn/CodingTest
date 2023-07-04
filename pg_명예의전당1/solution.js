c8d995a3f32eace9e3df9c9ed1a8234b995c67b5
pearlKinn <jinju000202@naver.com>
2023-07-04 09:56:49 +0900

    pg_명예의 전당 수정

--- pg_명예의전당1/solution.js
+++ pg_명예의전당1/solution.js
@@ -5,17 +5,15 @@ function solution(k, score) {
   for (let i = 0; i <score.length; i++) {
     if (rank.length < k) {
       rank.push(score[i]);
-      rank.sort((a, b) => b - a);
-      answer.push(rank[rank.length - 1]);
     } else if (rank[rank.length - 1] < score[i]) {
       rank.pop();
       rank.push(score[i]);
-      rank.sort((a, b) => b - a);
-      answer.push(rank[rank.length - 1]);
     } else {
       answer.push(rank[rank.length - 1]);
       continue;
     }
+    rank.sort((a, b) => b - a);
+    answer.push(rank[rank.length - 1]);
   }
   return answer;
 }
